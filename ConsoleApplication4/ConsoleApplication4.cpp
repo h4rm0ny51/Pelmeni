088f7bbb28701fade699031e0d88c4e343d6ecd4
h4rm0ny51 <kandaurovv51@gmail.com>
2025-05-16 14:23:16 +0300

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,50 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    double Qf = af * Qp;
+    double nf = Qf / qf;
+
+    double Qt = (1 - af) * Qp;
+    double nt = Qt / qt;
+
+    int np_rounded = roundUp(np);
+    int nf_rounded = roundUp(nf);
+    int nt_rounded = roundUp(nt);
+
+    cout << "\nРезультаты:" << endl;
+    cout << "np (количество пельменей): " << np_rounded << endl;
+    cout << "nf (количество фарша): " << nf_rounded << endl;
+    cout << "nt (количество теста): " << nt_rounded << endl;
+
+    return 0;
+}
